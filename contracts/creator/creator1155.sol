6a4bd82388477a08930692519b6e049d9a2aa035
OpenKoo <openkoodao@gmail.com>
2022-04-26 15:43:53 +0800

    merge

--- contracts/creator/creator1155.sol
+++ contracts/creator/creator1155.sol
@@ -1,13 +1,15 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+//   SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
 import "../token/OERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
 
 contract creator1155 is Ownable {
 
     mapping(address => address) public user_token;
     constructor()  {
     }
+
     address public new1155Token;
     function creat1155Token(address user) public returns(address){
         address token = address(new OERC1155(user));
--- contracts/token/OERC1155.sol
+++ contracts/token/OERC1155.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: GPL-3.0-or-later
+//  SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
@@ -11,12 +11,12 @@ contract OERC1155 is ERC1155{
     uint256 public constant SWORD = 3;
     uint256 public constant SHIELD = 4;
 
-    constructor (address creator_user) ERC1155("TOKEN_1155") {
-        _mint(creator_user, GOLD, 1, "");
-        _mint(creator_user, SILVER, 1, "");
-        _mint(creator_user, THORS_HAMMER, 1, "");
-        _mint(creator_user, SWORD, 1, "");
-        _mint(creator_user, SHIELD, 1, "");
+    constructor (address userAddr) ERC1155("TOKEN_1155") {
+        _mint(userAddr, GOLD, 10**18, "");
+        _mint(userAddr, SILVER, 10**27, "");
+        _mint(userAddr, THORS_HAMMER, 1, "");
+        _mint(userAddr, SWORD, 10**9, "");
+        _mint(userAddr, SHIELD, 10**9, "");
 
     }
 
